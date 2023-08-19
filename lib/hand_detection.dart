4bb2aa549bd00ba67252fca2adbb8124e258024f
Diaz Octa Pratama <135936128+diazoct@users.noreply.github.com>
2023-08-19 12:27:28 +0700

    Add files via upload

---
+++ lib/hand_detection.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HandDetectionScreen extends StatefulWidget {
+  const HandDetectionScreen({super.key});
+
+  @override
+  State<HandDetectionScreen> createState() => _HandDetectionScreenState();
+}
+
+class _HandDetectionScreenState extends State<HandDetectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
